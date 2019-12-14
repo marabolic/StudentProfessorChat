c06b5f4e24a4c1a9663bf3202fe6b97f8f13efc4
Mara <38396069+marabolic@users.noreply.github.com>
2019-12-14 14:16:36 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,60 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-package student;
+package profesor;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSConsumer;
-import javax.jms.JMSContext;
-import javax.jms.JMSException;
-import javax.jms.JMSProducer;
-import javax.jms.Message;
-import javax.jms.Queue;
-import javax.jms.TextMessage;
-import javax.jms.Topic;
+import javax.jms.*;
 
 
-/**
- *
- * @author makib
- */
-
-
-class Student{
-    
-    List subjs = new ArrayList<String>();
-    
+class Professor {
     private String subjectID;
 
-    public Student(String subjId) {
+    public Professor(String subjId) {
         subjectID = subjId;
     }
     
     public String getSubjectID(){ return subjectID;}
     
-   boolean contains(String s){
-       return subjs.contains(s);
-   } 
-   
-   void removeSubj(String s){
-       subjs.remove(s);
-   }
-   void addSubj(String s){
-       subjs.add(s);
-   }
-    
 }
 
-
 public class Main {
+     
     @Resource(lookup="jms/__defaultConnectionFactory")
     public static ConnectionFactory factory;
     
@@ -64,45 +29,53 @@ public class Main {
     @Resource(lookup="myTopic")
     public static Topic topic;
     
-    
-    
-    private static final int id = 1;
+    private String subjectID;
     
     public static void main(String[] args) {
-        try {
-            JMSContext context = factory.createContext();
-            context.setClientID(""+id);
-            JMSProducer producer = context.createProducer(); 
-            Student s = new Student("OOP");
-            JMSConsumer consumer = context.createDurableConsumer(topic,s.getSubjectID() );
-            // p.start();
-            TextMessage receivedTmsg = null;
-            
-            String text = new String("Q"+id);
-            
-            TextMessage questionTmsg = context.createTextMessage();
-            questionTmsg.setText(text);
-            
-            questionTmsg.setStringProperty("subject", "OOP"); //iz txtbox
-            
-            producer.send(queue, questionTmsg);
-            System.out.println("Postavljam pitanje: " + questionTmsg.getText());
-            
-            //todo: filter msg for subj
-            
-            Message receivedMsg = consumer.receive();
-            if (receivedMsg instanceof TextMessage){
-                try {
-                    receivedTmsg = (TextMessage)receivedMsg;
-                    System.out.println("Pitanje: " + questionTmsg.getText() + " Odgovor je: " + receivedTmsg.getText());
-                } catch (JMSException ex) {
-                    
-                }
+        
+        JMSContext context = factory.createContext();
+        JMSProducer producer = context.createProducer();
+        
+        //kad se loguje novi
+        Professor p = new Professor("OOP");
+        
+        
+        JMSConsumer consumer = context.createConsumer(queue);
+        TextMessage receivedTmsg = null;
+       
+        
+        System.out.println("tu sam");
+        Message receivedMsg = consumer.receive();
+        //todo: proveri property
+        
+        if (receivedMsg instanceof TextMessage){
+            try {
+                receivedTmsg = (TextMessage)receivedMsg;
+                System.out.println("Pitanje je: " + receivedTmsg.getText() + " subject= " + receivedTmsg.getStringProperty("subject"));
+            } catch (JMSException ex) {
+               
             }
+        }
+        
+        try {
+
+            Thread.sleep(1000*((int)(Math.random() * 6) + 5));
+            String ans = new String(receivedTmsg.getText() + "A");
+
+            TextMessage replyMsg = context.createTextMessage();
+            replyMsg.setText(ans);
+
+            replyMsg.setStringProperty("subject", receivedTmsg.getStringProperty("subject"));
+            producer.send(topic, replyMsg);
+            System.out.println("Odgovaram: " + replyMsg.getText());
+
+
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         } catch (JMSException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
     }
     
 }
+
